cd510ceb59eb2b5c8afa212d094a02b0c4baeb95
alwalker21@gmail.com <alwalker21@gmail.com>
2023-01-02 10:29:41 -0600

    Chapter 10

---
+++ ch10/10-1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+    ch1 := make(chan int)
+    ch2 := make(chan int)
+    go func() {
+        v := 1
+        ch1 <- v
+        v2 := <-ch2
+        fmt.Println(v, v2)
+    }()
+    v := 2
+    // ch2 <- v
+    v2 := <-ch1
+	ch2 <- v
+    fmt.Println(v, v2)
+}
